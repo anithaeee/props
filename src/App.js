a2f18f0126e868af2d31ff2559760ec79e48742a
anithaeee <anithaeee224@gmail.com>
2023-12-12 12:56:00 +0800

    dec12th

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Child from './Child';
 import { useState } from 'react';
 function App(props) {
   const data = [{name:'Anitha',Age:29,Place:'Hyderabad'},
-               {name:'Ashiwini',Age:24,Place:'Nalgonda'}]
+               {name:'Ashiwini',Age:24,Place:'Nalgonda'},
+               {name:'Mamata',Age:26,Place:'Kalwakurthiy'}]
   return (
     
       <>
